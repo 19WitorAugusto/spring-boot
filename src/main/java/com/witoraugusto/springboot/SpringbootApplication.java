c7ce56206be21b372cd11290686cca5653705b2a
Witor Augusto <witorsantosaugusto@gmail.com>
2021-12-11 15:30:51 -0300

    Criando operação de instanciacao

--- src/main/java/com/witoraugusto/springboot/SpringbootApplication.java
+++ src/main/java/com/witoraugusto/springboot/SpringbootApplication.java
@@ -1,13 +1,32 @@
 package com.witoraugusto.springboot;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.witoraugusto.springboot.domain.Categorie;
+import com.witoraugusto.springboot.repositories.CategorieRepository;
+
 @SpringBootApplication
-public class SpringbootApplication {
+public class SpringbootApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategorieRepository categorieRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categorie cat1 = new Categorie(null, "Informática");
+		Categorie cat2 = new Categorie(null, "Escritório");
+
+		categorieRepository.saveAll(Arrays.asList(cat1, cat2));
+
 	}
 
 }
