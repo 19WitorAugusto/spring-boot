31277a5f97f912a6b59f2324ddf031087cd4c6fd
Witor Augusto <witorsantosaugusto@gmail.com>
2021-12-12 15:14:44 -0300

    Ajustes no endpoint /categoria/{id}

--- src/main/java/com/witoraugusto/springboot/resource/ProdutoResource.java
+++ src/main/java/com/witoraugusto/springboot/resource/ProdutoResource.java
@@ -1,5 +1,24 @@
 package com.witoraugusto.springboot.resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.witoraugusto.springboot.domain.Produto;
+import com.witoraugusto.springboot.services.ProdutoService;
+
+@RestController
+@RequestMapping(value = "/produto")
 public class ProdutoResource {
 
+	@Autowired
+	private ProdutoService service;
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Produto> findById(Integer id) {
+		Produto obj = service.findById(id);
+		return ResponseEntity.ok(obj);
+	}
 }
