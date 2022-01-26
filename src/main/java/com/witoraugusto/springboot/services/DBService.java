134c8ec44254dce188af28ed14151549f002f986
witor <witorsantosaugusto.com>
2022-01-25 21:13:19 -0300

    Implementando SmtpEmailService com servidor do google

--- src/main/java/com/witoraugusto/springboot/services/DBService.java
+++ src/main/java/com/witoraugusto/springboot/services/DBService.java
@@ -112,7 +112,7 @@ public class DBService {
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
 
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "witorsantosaugusto@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
 		cli1.getTelefones().addAll(Arrays.asList("2763323", "93838393"));
 
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "apto 203", "Jardim", "38220834", cli1, c1);
